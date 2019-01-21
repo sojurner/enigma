b06ddf90a1138619b48030936a20500af629e8ce
sojurner <pykim0591@gmail.com>
2019-01-21 06:18:00 -0700

    Add enigma test

---
+++ test/enigma_test.rb
@@ -0,0 +1,37 @@
+require_relative 'test_helper'
+require './lib/enigma'
+require './lib/alpha_numeric_date'
+
+class EnigmaTest < Minitest::Test
+  include AlphaNumericDate
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_can_encrypt_messages
+    result = @enigma.encrypt('hello world', "02715", "040895")
+
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    
+    assert_equal expected, result
+  end
+
+  def test_it_can_decrypt_messages
+    result = @enigma.decrypt('keder ohulw', '02715', '040895')
+
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+  end
+end
