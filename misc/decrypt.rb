29f17041a58d92eb8c12e9aa2f038936392c4b70
Paul Kim <pykim0591@gmail.com>
2019-01-26 17:15:20 -0700

    Link encrypted to enigma file

--- misc/decrypt.rb
+++ misc/decrypt.rb
@@ -1,4 +1,8 @@
+require './lib/enigma'
 require 'pry'
 require 'csv'
 
-x = CSV.read(ARGV[0])
+message = CSV.read(ARGV[0]).flatten
+enigma = Enigma.new
+encrypted = enigma.encrypt(message[0], message[1], message[2])
+puts "Created #{ARGV[1]} with the key #{encrypted[:key]} and date #{encrypted[:date]}"
