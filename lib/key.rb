54934a31107930ed036f77a90f4855a90c4d3961
Paul Kim <pykim0591@gmail.com>
2019-02-21 15:01:27 -0700

    Add rotate_keys and refine keys method

--- lib/key.rb
+++ lib/key.rb
@@ -16,6 +16,16 @@ class Key
     join_keys(parsed_keys, shift)
   end
 
+  def rotate_keys(key_set)
+    key_set.map.with_index do |char, index| 
+      next_key = key_set[index+ 1] 
+      "#{key_set[index]}#{key_set[index+1]}" if next_key
+    end
+  end
+
+  def refine_keys(keys)
+    keys.reject {|e| e.to_s.empty?}
+  end
 
     keys.map.with_index {|key, index|
       key.to_i + shift[index].to_i
