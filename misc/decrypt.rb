cae0fee7b883755cd010791513230e5f3d480a16
Paul Kim <pykim0591@gmail.com>
2019-02-21 15:06:25 -0700

    Add decrypt file

--- misc/decrypt.rb
+++ misc/decrypt.rb
@@ -2,7 +2,15 @@ require './lib/enigma'
 require 'pry'
 require 'csv'
 
-message = CSV.read(ARGV[0]).flatten
+file_reader = File.open(ARGV[0], 'r')
+message = file_reader.read.split(",")
 enigma = Enigma.new
 encrypted = enigma.encrypt(message[0], message[1], message[2])
+
+file_writer = File.open(ARGV[1], 'w')
+file_writer.write(encrypted)
+
 puts "Created #{ARGV[1]} with the key #{encrypted[:key]} and date #{encrypted[:date]}"
+
+file_reader.close
+file_writer.close
