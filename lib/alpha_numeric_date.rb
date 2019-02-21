86ec3082dcf4ea4ce787ba41c604b2033906819b
Paul Kim <pykim0591@gmail.com>
2019-02-21 14:55:52 -0700

    Generate random five digit key

--- lib/alpha_numeric_date.rb
+++ lib/alpha_numeric_date.rb
@@ -7,7 +7,7 @@ module AlphaNumericDate
 
   def random_five_digit_key
     random_five_digit = []
-    5.times{ random_five_digit << rand(0..9)}
+    5.times{ random_five_digit << rand(0..9) }
     random_five_digit.join
   end
 
