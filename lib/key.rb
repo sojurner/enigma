36c699b2596a4309ccc5a23412effba9326091e4
Paul Kim <pykim0591@gmail.com>
2019-02-21 15:00:50 -0700

    Add generate encrypted keys method

--- lib/key.rb
+++ lib/key.rb
@@ -10,12 +10,12 @@ class Key
   end
   
   def generate_encrypted_keys(shift)
-    individual_key = @key.split(//) 
-    keys = individual_key.map.with_index { |char, index| 
-      individual_key[index+ 1] ? 
-      "#{individual_key[index]}#{individual_key[index+1]}" : 
-      nil
-    }.reject {|e| e.to_s.empty?}
+    key_set = @key.chars 
+    adjusted_keys = rotate_keys(key_set)
+    parsed_keys = refine_keys(adjusted_keys)
+    join_keys(parsed_keys, shift)
+  end
+
 
     keys.map.with_index {|key, index|
       key.to_i + shift[index].to_i
