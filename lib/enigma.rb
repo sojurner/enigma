e49957fccb967c0bd8c5b1597e430a578e091f46
Paul Kim <pykim0591@gmail.com>
2019-02-21 20:46:51 -0700

    Add create methods for each separate class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,8 +3,6 @@ require './lib/date_shift'
 require './lib/message'
 
 class Enigma
-  include AlphaNumericDate
-
   attr_reader :message, 
               :key, 
               :date
@@ -16,42 +14,39 @@ class Enigma
   end
 
   def encrypt(message, key = nil, date = nil)
-    @message = Message.new(message)
-    !date ? @date = DateShift.new : @date = DateShift.new(date)
-    !key ? @key = Key.new : @key = Key.new(key)
-    direct_message('encrypt')
+    create_message(message)
+    create_date(date)
+    create_key(key)
+    create_encryption_or_decryption('encrypt')
   end
 
   def decrypt(cipher_message, key = nil, date = nil)
-    @message = Message.new(cipher_message)
+    create_message(cipher_message)
+    create_date(date)
+    create_key(key)
+    create_encryption_or_decryption('decrypt')
+  end
+  
+  def create_message(message)
+    @message = Message.new(message)
+  end
+
+  def create_date(date)
     !date ? @date = DateShift.new : @date = DateShift.new(date)
+  end
+
+  def create_key(key)
     !key ? @key = Key.new : @key = Key.new(key)
-    direct_message('decrypt')
   end
 
-  def crack(cipher_msg, key = nil, date)
-    @message = Message.new(cipher_msg)
-    !date ? @date = DateShift.new : @date = DateShift.new(date)
-    shift = @date.date_offset.map {|letter| letter.to_i}
-    alphabet_index
-    require 'pry'; binding.pry
-    @message.encrypt_decrypt_characters(x, 'encrypt')
-  end
-
-  def last_four_keys(shift, message)
-    require 'pry'; binding.pry
-    last_four = message[message.length - 4..-1].chars
-    last_four.map {|letter| alphabet.index(letter)}
-  end
-
-  def direct_message(command)
+  def create_encryption_or_decryption(command)
     shift = @date.date_offset
     keys = @key.generate_encrypted_keys(shift)
     code = @message.encrypt_decrypt_characters(keys, command)
-    create_encryption(command, code)
+    encrypted_hash(command, code)
   end
 
-  def create_encryption(command, code)
+  def encrypted_hash(command, code)
     code_hash = {
       decryption: (code if command == "decrypt"),
       encryption: (code if command == "encrypt"),
@@ -66,13 +61,24 @@ class Enigma
     code_hash
   end
 
-  def known_indices
-    known_chars = " end".chars
-    known_chars.map {|letter| alphabet.index(letter)}
-  end
+  # def last_four_keys(shift, message)
+  #   require 'pry'; binding.pry
+  #   last_four = message[message.length - 4..-1].chars
+  #   last_four.map {|letter| alphabet.index(letter)}
+  # end
 
-  def decrypt_date(key, offset)
-    known_indices.map.with_index {|num, index| num - }
-  end
+  # def crack(cipher_msg, key = nil, date)
+  #   @message = Message.new(cipher_msg)
+  #   !date ? @date = DateShift.new : @date = DateShift.new(date)
+  #   shift = @date.date_offset.map {|letter| letter.to_i}
+  #   alphabet_index
+  #   require 'pry'; binding.pry
+  #   @message.encrypt_decrypt_characters(x, 'encrypt')
+  # end
+
+  # def known_indices
+  #   known_chars = " end".chars
+  #   known_chars.map {|letter| alphabet.index(letter)}
+  # end
 
 end
