e6802fc9f9c13ceb6867ebda11aeda807dab53c3
Paul Kim <pykim0591@gmail.com>
2019-02-22 08:36:21 -0700

    Migrate class instances to seperate files

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -14,39 +14,27 @@ class Enigma
   end
 
   def encrypt(message, key = nil, date = nil)
-    create_message(message)
-    create_date(date)
-    create_key(key)
+    @message = Message.create_message(message)
+    @date = DateShift.create_date(date)
+    @key = Key.create_key(key)
     create_encryption_or_decryption('encrypt')
   end
 
   def decrypt(cipher_message, key = nil, date = nil)
-    create_message(cipher_message)
-    create_date(date)
-    create_key(key)
+    @message = Message.create_message(cipher_message)
+    @date = DateShift.create_date(date)
+    @key = Key.create_key(key)
     create_encryption_or_decryption('decrypt')
-  end
-  
-  def create_message(message)
-    @message = Message.new(message)
-  end
-
-  def create_date(date)
-    !date ? @date = DateShift.new : @date = DateShift.new(date)
-  end
-
-  def create_key(key)
-    !key ? @key = Key.new : @key = Key.new(key)
   end
 
   def create_encryption_or_decryption(command)
     shift = @date.date_offset
     keys = @key.generate_encrypted_keys(shift)
     code = @message.encrypt_decrypt_characters(keys, command)
-    encrypted_hash(command, code)
+    cipher_hash(command, code)
   end
 
-  def encrypted_hash(command, code)
+  def cipher_hash(command, code)
     code_hash = {
       decryption: (code if command == "decrypt"),
       encryption: (code if command == "encrypt"),
@@ -80,5 +68,4 @@ class Enigma
   #   known_chars = " end".chars
   #   known_chars.map {|letter| alphabet.index(letter)}
   # end
-
 end
