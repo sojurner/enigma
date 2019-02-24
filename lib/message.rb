927c5e12e8da40c4e354eb460162809517126ab6
Paul Kim <pykim0591@gmail.com>
2019-02-23 20:17:52 -0700

    Rename variables

--- lib/message.rb
+++ lib/message.rb
@@ -18,26 +18,27 @@ class Message
     string_arr
   end
 
-  def encrypt_decrypt_characters(key, command)
+  def shift_grouped_values(string_arr, key_shift, command)
+    string_arr.map.each_with_index do |char, index|
+      shift_letter(char, key_shift[index], command)
+    end.flatten
+  end
+
+  def encrypt_decrypt_characters(key_shift, command)
     grouped_message.map do |string_arr|
-      string_arr.map.each_with_index do |char, index|
-        shift_letter(char, key[index], command)
-      end.flatten
+      shift_grouped_values(string_arr, key_shift, command)
     end.join('')
   end
 
-  def shift_letter(letter, shift, command)
+  def shift_letter(letter, shift_val, command)
     initial_index = alphabet.index(letter.downcase)
-    shifted_alphabet = control_cipher(shift, command)
+    shifted_alphabet = control_cipher(shift_val, command)
     shifted_alphabet[initial_index]
   end
 
-  def control_cipher(shift, command) 
-    case(command)
-    when "encrypt"
-      alphabet.rotate(shift.to_i)
-    else
-      alphabet.rotate(shift.to_i * -1) 
-    end
+  def control_cipher(shift_val, command) 
+    command == "encrypt" ? 
+    alphabet.rotate(shift_val.to_i) :
+    alphabet.rotate(shift_val.to_i * -1) 
   end
 end
