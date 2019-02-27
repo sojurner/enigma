278e9841930e1090a9fdb663af2da5fc41c4b0f5
Paul Kim <pykim0591@gmail.com>
2019-02-27 10:52:28 -0700

    Fix decrpyt and encrypt files

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -3,13 +3,12 @@ require 'pry'
 require 'csv'
 
 file_reader = File.open(ARGV[0], 'r')
-message = file_reader.read.split(",")
+message = file_reader.read.split(/\n/)
 enigma = Enigma.new
-decrypt = enigma.decrypt(message[0], message[1], message[2])
+decrypt = enigma.decrypt(message[0], ARGV[2], ARGV[3])
 
 file_writer = File.open(ARGV[1], 'w')
-file_writer.write(decrypt)
-
+file_writer.write(decrypt[:decryption])
 puts "Created #{ARGV[1]} with the key #{decrypt[:key]} and date #{decrypt[:date]}"
 
 file_reader.close
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,12 +3,12 @@ require 'pry'
 require 'csv'
 
 file_reader = File.open(ARGV[0], 'r')
-message = file_reader.read.split(",")
+message = file_reader.read.split(/\n/)
 enigma = Enigma.new
-encrypted = enigma.encrypt(message[0], message[1], message[2])
-
+encrypted = enigma.encrypt(message[0], ARGV[2], ARGV[3])
 file_writer = File.open(ARGV[1], 'w')
-file_writer.write(encrypted)
+file_writer.write(encrypted[:encryption])
+
 
 puts "Created #{ARGV[1]} with the key #{encrypted[:key]} and date #{encrypted[:date]}"
 
